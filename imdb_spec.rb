f90a407de93f6b7bfe69bcc3429bfab983e03b53
Sergio Gil <sgilperez@gmail.com>
2008-08-30 17:23:21 +0200

    Specs refactoring

--- imdb_spec.rb
+++ imdb_spec.rb
@@ -6,13 +6,13 @@ describe ImdbSearch do
     @imdb_search = ImdbSearch.new('Indiana Jones')
     @imdb_search.stub!(:open).and_return(open('sample_search.html'))
   end
+  
+  it "should query IMDB url" do
+    @imdb_search.should_receive(:open).with("http://www.imdb.com/find?s=all&q=Indiana+Jones").and_return(open('sample_search.html'))
+    @imdb_search.send(:document)
+  end
 
   describe "movies" do
-
-    it "should query IMDB url" do
-      @imdb_search.should_receive(:open).with("http://www.imdb.com/find?s=all&q=Indiana+Jones").and_return(open('sample_search.html'))
-      @imdb_search.movies
-    end
 
     it "should be a collection of ImdbMovie instances" do
       @imdb_search.movies.should have(59).imdb_movies
@@ -22,12 +22,18 @@ describe ImdbSearch do
       @imdb_search.movies.detect { |m| m.title == 'Indiana Jones and the Last Crusade' }.should_not be_nil
     end
 
-    it "should have titles (without HTML tags)" do
+    it "should have titles" do
       @imdb_search.movies.each do |movie|
         movie.title.should_not be_empty
+      end
+    end
+    
+    it "should not have titles with HTML tags" do
+      @imdb_search.movies.each do |movie|
         movie.title.should_not match(/<.+>/)
       end
     end
+    
   end
 
 end

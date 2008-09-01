930e69ad0e914fa58e0d6860782a40bf619e2e7f
Sergio Gil <sgilperez@gmail.com>
2008-09-01 12:56:30 +0200

    Small refactoring

--- spec/imdb_search_spec.rb
+++ spec/imdb_search_spec.rb
@@ -2,38 +2,42 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe ImdbSearch do
 
-  before(:each) do
-    @imdb_search = ImdbSearch.new('Indiana Jones')
-    @imdb_search.stub!(:open).and_return(open("#{$samples_dir}/sample_search.html"))
-  end
-  
-  it "should query IMDB url" do
-    @imdb_search.should_receive(:open).with("http://www.imdb.com/find?s=all&q=Indiana+Jones").and_return(open("#{$samples_dir}/sample_search.html"))
-    @imdb_search.send(:document)
-  end
+  describe 'Indiana Jones' do
 
-  describe "movies" do
-
-    it "should be a collection of ImdbMovie instances" do
-      @imdb_search.movies.should have(59).imdb_movies
+    before(:each) do
+      @imdb_search = ImdbSearch.new('Indiana Jones')
+      @imdb_search.stub!(:open).and_return(open("#{$samples_dir}/sample_search.html"))
     end
 
-    it "should include 'Indiana Jones and the Last Crusade'" do
-      @imdb_search.movies.detect { |m| m.title == 'Indiana Jones and the Last Crusade' }.should_not be_nil
+    it "should query IMDB url" do
+      @imdb_search.should_receive(:open).with("http://www.imdb.com/find?s=all&q=Indiana+Jones").and_return(open("#{$samples_dir}/sample_search.html"))
+      @imdb_search.send(:document)
     end
 
-    it "should have titles" do
-      @imdb_search.movies.each do |movie|
-        movie.title.should_not be_empty
+    describe "movies" do
+
+      it "should be a collection of ImdbMovie instances" do
+        @imdb_search.movies.should have(59).imdb_movies
       end
-    end
-    
-    it "should not have titles with HTML tags" do
-      @imdb_search.movies.each do |movie|
-        movie.title.should_not match(/<.+>/)
+
+      it "should include 'Indiana Jones and the Last Crusade'" do
+        @imdb_search.movies.detect { |m| m.title == 'Indiana Jones and the Last Crusade' }.should_not be_nil
       end
+
+      it "should have titles" do
+        @imdb_search.movies.each do |movie|
+          movie.title.should_not be_empty
+        end
+      end
+
+      it "should not have titles with HTML tags" do
+        @imdb_search.movies.each do |movie|
+          movie.title.should_not match(/<.+>/)
+        end
+      end
+
     end
-    
+
   end
 
 end
